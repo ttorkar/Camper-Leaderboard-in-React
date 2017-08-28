4b7024f55afbe4539c89b5019c7ad1bcc2e5fe76
unknown <trent@torkar.me>
2017-08-28 15:16:36 +1000

    Camper Leaderboard in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import CamperList from "./camper_list"
+import MDSpinner from 'react-md-spinner';
 
 export default class App extends Component {
+  constructor(props){
+  	super(props);
+  	this.state = {
+  		recentCampers: [],
+  		allTimeCampers: [],
+  		currentView: 'allTimeCampers'
+  	}
+  }
+
+  componentWillMount() {
+  	// Concurrent Requests + Set State to Response
+  	axios.all([this.fetchRecentCampers(),this.fetchAllTimeCampers()])
+  	.then(axios.spread((recentCampers, allTimeCampers)=>{
+  		this.setState({
+        recentCampers:recentCampers.data,
+        allTimeCampers:allTimeCampers.data
+   		});
+  	}));
+  }
+
+  fetchRecentCampers() {
+  	//Fetch the Recent Campers
+  	return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+  }
+
+    fetchAllTimeCampers() {
+  	//Fetch the Top Campers
+  	return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+  }
+
+  changeView(view) {
+    this.setState({currentView:view});
+  }
+
   render() {
+    if (!this.state.recentCampers.length && !this.state.allTimeCampers.length){
+      return <MDSpinner className="spinner" size={200}/>
+    }
     return (
-      <div>React simple starter</div>
+      <div class="heading">
+    	<div>
+      <div><h2>Leaderboard</h2></div>
+      <h4>Viewing {this.state.currentView}</h4>
+      </div>
+      <button onClick={() => this.changeView('recentCampers')} className="btn btn-primary" id="30Days">Past 30 Days</button>
+      <button onClick={() => this.changeView('allTimeCampers')} className="btn btn-warning" id="top">All Time</button>
+      <CamperList campers={this.state[this.state.currentView]} />
+      </div>
     );
   }
 }
---
+++ src/components/camper_list.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import CamperListItem from "./camper_list_item"
+//campers = props.campers
+
+const CamperList = ({campers}) => {
+
+  const Items = campers.map((camper, index) => {
+    return (
+      <CamperListItem key={index} camper={camper} number={index + 1} />
+    );
+  });
+
+
+  return (
+    <table className="table table-striped">
+    <thead>
+    <tr>
+    <th>#</th>
+    <th>Username</th>
+    <th>Last 30 Days</th>
+    <th>All Time Points</th>
+    </tr>
+    </thead>
+    <tbody>
+      {Items}
+    </tbody>
+    </table>
+  );
+}
+
+export default CamperList;
